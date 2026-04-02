d1dc16bf0e96d1a89f528a2adccfd7cb2a4944f1
nguyenho <nguye@NGUYEN_HO>
2026-04-02 12:04:23 +0700

    Fix database timeout for deploy

--- backend/app/config.py
+++ backend/app/config.py
@@ -9,11 +9,16 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 MODEL_NAME = "models/gemini-2.5-flash"
 
 def get_connection():
-    conn = pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=NGUYEN_HO\\SQLEXPRESS;"
-        "DATABASE=ChatbotLuatHonNhan;"
-        "UID=sa;"
-        "PWD=123123"
-    )
-    return conn
+    try:
+        conn = pyodbc.connect(
+            "DRIVER={ODBC Driver 17 for SQL Server};"
+            "SERVER=NGUYEN_HO\\SQLEXPRESS;"
+            "DATABASE=ChatbotLuatHonNhan;"
+            "UID=sa;"
+            "PWD=123123",
+            timeout=5 # Giới hạn thời gian đợi 5 giây thôi
+        )
+        return conn
+    except:
+        # Nếu lỗi (do đang chạy trên Cloud), trả về None để app chạy tiếp
+        return None
